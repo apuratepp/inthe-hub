dbc9f5eefeb0d0b9a88db315758599e860a267f4
Josep <apuratepp@gmail.com>
2012-12-30 21:45:58 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntheHub::Application.config.secret_token = '8209002f689f497890516483d9e060e6e30d4d1d2b4ecffc8892ddf1e53bb50ef05be079537a76fc4aa78020dd144e951b8dc1e612a920722e9b5a29a7c0e90a'
