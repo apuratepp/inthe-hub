c928bcfc2bb425a3ca263775a6f0eb0682af6083
Josep <apuratepp@gmail.com>
2013-01-01 17:40:05 +0100

    repos path yaml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+REPOS_CONFIG = YAML.load_file(Rails.root.join('config', 'repos.yml'))[Rails.env]
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,20 +1,23 @@
 class Repo < ActiveRecord::Base
   attr_accessible :description, :name, :path
   
-  REPO_PATH = '/Users/apuratep/Developer/code/inthe'
+  REPO_DIRS = REPOS_CONFIG['repo_dirs']
   
   def rug
     Rugged::Repository.new(self.path)
   end
   def self.list
   	repos = []
-  	Dir.entries(REPO_PATH).each do |filename|
-  		path = File.join(REPO_PATH, filename)
-  		begin
-	  		repos << Rugged::Repository.new(path)
-	  	rescue
-	  		# not a git repo
-	  	end
+
+  	REPO_DIRS.each do |dir|
+      Dir.entries(dir).each do |filename|
+  		  path = File.join(dir, filename)
+  		  begin
+	  	  	repos << Rugged::Repository.new(path)
+	  	  rescue
+	  	  	# not a git repo
+	  	  end
+      end
   	end
   	repos
   end
