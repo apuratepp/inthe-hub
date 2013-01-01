527a20894c03290e2cd3dc37fce075f89afce153
Josep <apuratepp@gmail.com>
2013-01-01 16:32:30 +0100

    automatic list repos of a directory

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+  	@repos = Repo.list
   end
 end
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,12 +1,22 @@
 class Repo < ActiveRecord::Base
   attr_accessible :description, :name, :path
   
+  REPO_PATH = '/Users/apuratep/Developer/code/inthe'
   
   def rug
     Rugged::Repository.new(self.path)
   end
   def self.list
-  	Dir.entries('/Users/apuratep/Developer/code/inthe')
+  	repos = []
+  	Dir.entries(REPO_PATH).each do |filename|
+  		path = File.join(REPO_PATH, filename)
+  		begin
+	  		repos << Rugged::Repository.new(path)
+	  	rescue
+	  		# not a git repo
+	  	end
+  	end
+  	repos
   end
 
 end
