81aa36ef845ef1755eab339953fc12bc39a4c3c6
Josep <jsirvent@inthe.es>
2013-01-04 16:25:23 +0100

    repos reverse

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-  	@repos = Repo.list
+  	@repos = Repo.list.sort_by {|repo| Time.at(repo.lookup(repo.head.target).time) }
+  	@repos.reverse!
   end
   def repo
   	@repo = Repo.rug(params[:path])
