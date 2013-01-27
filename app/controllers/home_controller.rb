003e0e5541876cd15d78896bbd62a45547bd8b0b
Josep <apuratepp@gmail.com>
2013-01-27 17:20:33 +0100

    canvi ordre dels repos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
-  	@repos = Repo.list.sort_by {|repo| Time.at(repo.lookup(repo.head.target).time) }
-  	@repos.reverse!
+  	# @repos = Repo.list.sort_by {|repo| Time.at(repo.lookup(repo.head.target).time) }
+  	@repos = Repo.list.sort_by {|repo| File.basename(repo.workdir)}
+  	# @repos.reverse!
   end
   def repo
   	@repo = Repo.rug(params[:path])
